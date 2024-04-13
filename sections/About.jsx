782cc9d39e25737719cdb71f1a01b98d67772729
Harlequin <hideki4x7@tutanota.com>
2024-04-13 12:51:57 +0200

    Customized Hero and About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,44 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| Über das Metaverse" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Metaverse </span>
+        ist die Zukunft, wo Sie die virtuelle Welt genießen können, indem Sie
+        die Realität fühlen. Erkunde mit einem VR-Gerät das Metaversum und
+        verwandle deine Träume in Realität um. Lassen Sie uns die
+        <span className="font-extrabold text-white">
+          {' '}
+          Unendlichkeit des Metaversums{' '}
+        </span>
+        erkunden, indem Sie nach unten scrollen
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
   </section>
 );
 
